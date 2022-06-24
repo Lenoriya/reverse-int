43cbbdaf3b2a8b80cbaee53e958a023c8fcda9b3
Helen Kruminia <lena_kru@tut.by>
2022-06-24 15:46:03 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let rev, s = '';
+    rev = n.toString();
+
+    for (let i = rev.length-1; i >= 0; i--) {
+        s += rev[i];
+    }
+    return parseInt(s);
 }
